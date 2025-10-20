3b8dd05304e43e132925c82564bd6658f71fa2e8
Milo Canseco <canseco3rd@gmail.com>
2025-10-19 20:53:28 -0700

    Created guards for voting age, multiplication, and concatenateStrings.

--- patterns/guard.js
+++ patterns/guard.js
@@ -8,6 +8,10 @@ export function multiply(num1, num2) {
   if (typeof num1 !== "number" || typeof num2 !== "number") {
     return NaN;
   }
+
+  if (num1 === 0 || num2 === 0) {
+    return 0;
+  }
   return num1 * num2;
 }
 
@@ -18,7 +22,13 @@ export function multiply(num1, num2) {
  * @returns {string} "Who would you like to vote for?" if `age` is 18 or older
  */
 export function vote(age) {
-  // TODO
+  if (typeof age !== "number") {
+    return "Age not valid.";
+  }
+  if (age < 18) {
+    return "You must be 18 or older to vote.";
+  }
+  return "Who would you like to vote for?";
 }
 
 /**
@@ -28,5 +38,8 @@ export function vote(age) {
  * @returns `undefined` if either argument is not a string
  */
 export function concatenateStrings(str1, str2) {
-  // TODO
+  if (typeof str1 !== "string" || typeof str2 !== "string") {
+    return undefined;
+  }
+  return str1 + str2;
 }
